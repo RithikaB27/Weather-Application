e2f3dbcd29f04f8639180a8788be247f78a278a5
Rithika B <brithika906@gmail.com>
2024-06-13 18:22:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://rithikab27.github.io/Weather-Application/',  
+  base: '/Weather-Application/',  
 })
 
