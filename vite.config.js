c9f3c03978f2417544159159cfc4b626cc9f9684
Rithika B <brithika906@gmail.com>
2024-06-13 17:41:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://weatherapplication27.netlify.app',  // Make sure this is set correctly
+  base: '/https://weatherapplication27.netlify.app',  
 })
