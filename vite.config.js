136d83a95a1a14baa71a843310676eef50121aa5
Rithika B <brithika906@gmail.com>
2024-06-13 17:47:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000,
-  }
+  base: '/https://weatherapplication27.netlify.app',  // Make sure this is set correctly
 })
